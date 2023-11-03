72396d1ef66341e2c9cbbaba18790325e3609323
Andrew Acheampong <gorgi.andy@gmail.com>
2023-11-03 03:07:12 +0300

    Initial Commit

---
+++ Instagram-UI (Figma)/Instagram_UI__Figma_App.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_UI__Figma_App.swift
+//  Instagram-UI (Figma)
+//
+//  Created by Andrew Acheampong on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_UI__Figma_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
